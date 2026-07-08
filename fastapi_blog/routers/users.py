b74ae556a6ae2e0efa75e84e942e4d44ca83e823
Simon Peter <simonpi442@gmail.com>
2026-07-08 04:16:39 +0100

    reformat 1 file with black

--- fastapi_blog/routers/users.py
+++ fastapi_blog/routers/users.py
@@ -246,8 +246,8 @@ async def upload_profile_picture(
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=(
-               "Invalid image file. Please upload a valid image "
-               "(JPEG, PNG, GIF, WebP)."
+                "Invalid image file. Please upload a valid image "
+                "(JPEG, PNG, GIF, WebP)."
             ),
         ) from err
 
